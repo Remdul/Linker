65ec9cb2c36f1949845a883f91fc18718ceaa774
Bryan Elliott <Remdul@localhost.localdomain>
2014-08-21 15:50:17 -0400

    Rewrote from scratch. Using one class at this time as its easier for me to follow. Append/Prepend working. Delete only removes the first entry for now. Working on making it selectable.

--- Linker.cpp
+++ Linker.cpp
@@ -20,127 +20,157 @@ struct Node {
 	int _value;
 	Node *_nodeNext;
 	Node *_nodePrev;
-
-public:
-	Node(void);
-	Node(int val);
 };
 
-Node::Node(int val) {
-	_value = val;
-	_nodeNext = NULL;
-}
-
-Node::Node(void) {
-	_nodeNext = NULL;
-}
-
-
-
-
-class LinkedList {
-public:
-	LinkedList(void);
-	~LinkedList(void);
-	void print();
-	void append(int value);
-	void prepend(int value);
-	void add(int value);
-	bool isEmpty();
-private:
-	Node *_listStart;
-	Node *_listEnd;
-
-};
-
-bool LinkedList::isEmpty() {
-	if (_listStart == NULL) {
-		return true;
-	}
-	return false;
-}
-// Initializing Linked List
-LinkedList::LinkedList() {
-	_listStart = _listEnd = NULL;
-}
-
-LinkedList::~LinkedList() {
-
-}
-
-void LinkedList::append(int value) {
-	Node *node = new Node(value);
-	if (isEmpty()) {
-		mark();
-		_listStart = node;
-	} else {
-		mark();
-		_listEnd->_nodeNext = node;
-	}
-	_listEnd = node;
-}
-
-void LinkedList::prepend(int value) {
-	Node *node = new Node(value);
-	if (isEmpty()) {
-		_listStart = node;
-	} else {
-		_listStart->_nodeNext = node;
-	}
-	_listEnd = node;
-}
-
-void LinkedList::print() {
-	Node *p = _listStart;
-if (isEmpty()) {
-	cout << "There's nothing to print. The list is empty." << endl;
-	return;
-	}
-	cout << "Linked List: " << endl;
-	while (p != NULL) {
-		cout << p->_value << endl;
-		p = p->_nodeNext;
-	}
-	cout << endl;
-}
-
-void mainMenu(LinkedList &list) {
-	int add;
-	int arg2;
-	cout << "1) Print List." 				<< endl;
-	cout << "2) Append to List." 			<< endl;
-	cout << "3) Prepend to List." 			<< endl;
-	cout << "4) Delete from List." 			<< endl;
-	cout << "5) Quit" 						<< endl;
-	cout << "   ==================" 		<< endl;
-	cout << "What would you like to do: " 	<< endl;
-	cin >> add;
-
-	if (add == 1) {
-		list.print();
-	} else if (add == 2) {
-		cout << "Value to Append: ";
-		cin >> arg2;
-		list.append(arg2);
-	} else if (add == 3) {
-		cout << "Value to Prepend: ";
-		cin >> arg2;
-		list.prepend(arg2);
-	} else if (add == 4) {
-		cout << "Value to Delete: ";
-		cin >> arg2;
-		list.append(arg2);
-	} else if (add == 5) {
-		quit = true;
-	}
-
-}
+bool isEmpty(Node *head);
+char menu();
+void insert(Node * &head, Node * &last, int value);
+void prepend(Node * &head, Node * &last, int value);
+void append(Node * &head, Node * &last, int value);
+void remove(Node * &head, Node * &last);
+void print(Node *current);
 
 int main() {
-	LinkedList list;
+	Node *head = NULL;
+	Node *last = NULL;
+	char choice;
+	int number;
 
-	while (quit == false) {
-		mainMenu(list);
-	}
+	/*	cout << "1) Print List." << endl;
+	cout << "2) Add an Item to List." << endl;
+	cout << "3) Prepend an Item." << endl;
+	cout << "4) Remove an Item." << endl;
+	cout << "5) Exit Program." << endl;
+	 */
+	do{
+		choice = menu();
+		switch(choice)
+		{
+		case '1':
+			print(head);
+			break;
+		case '2':
+			cout << "Please enter a number: ";
+			cin >> number;
+			append(head, last, number);
+			break;
+		case '3':
+			cout << "Please enter a number: ";
+			cin >> number;
+			prepend(head, last, number);
+			break;
+		case '4':
+			remove(head,last);
+			break;
+		default:
+			cout << "Invalid Option!" << endl;
+
+		}
+
+	}while(choice != '5');
+
 	return 0;
+}
+
+
+bool isEmpty(Node *head)
+{
+	if(head == NULL){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+char menu(){
+	char choice;
+	cout << "Menu" << endl;
+	cout << "1) Print List." << endl;
+	cout << "2) Add an Item to List." << endl;
+	cout << "3) Prepend an Item." << endl;
+	cout << "4) Remove an Item." << endl;
+	cout << "5) Exit Program." << endl;
+
+	cin >> choice;
+	return choice;
+}
+
+void insert(Node * &head, Node * &last, int value){
+	Node *temp = new Node;
+	temp->_value = value;
+	temp->_nodeNext = NULL;
+	temp->_nodePrev = NULL;
+	head = temp;
+	last = temp;
+}
+
+void append(Node * &head, Node * &last, int value){
+	if(isEmpty(head))
+	{
+		insert(head, last, value);
+	}
+	else
+	{
+		Node *temp = new Node;
+		temp->_value = value;
+		temp->_nodeNext = NULL;
+		temp->_nodePrev = last;
+		last->_nodeNext = temp;
+		last = temp;
+	}
+}
+
+void prepend(Node * &head, Node * &last, int value){
+	if(isEmpty(head))
+	{
+		insert(head, last, value);
+	}
+	else
+	{
+		Node *temp = new Node;
+		temp->_value = value;
+		temp->_nodeNext = head;
+		temp->_nodePrev = NULL;
+		head->_nodePrev = temp;
+		head = temp;
+	}
+}
+
+void remove(Node * &head, Node * &last){
+	int line;
+	if(isEmpty(head))
+	{
+		cout << "This list is empty. No need to delete anything!" << endl;
+	}
+	else if (head == last)
+	{
+		delete head;
+		head == NULL;
+		last == NULL;
+	}
+	else
+	{
+		Node * temp = head;
+		head = head->_nodeNext;
+		delete temp;
+	}
+}
+
+void print(Node *current){
+	int i;
+	if (isEmpty(current))
+	{
+		cout << "The list is empty! :(" << endl;
+	}
+	else
+	{
+		cout << "This list contains: " << endl;
+
+		while (current != NULL)
+		{
+			cout << current->_value << endl;
+			current = current->_nodeNext;
+		}
+	}
 }
